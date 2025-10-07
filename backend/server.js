6d19081952de3030b6ecc997715d67ad2c480bd2
sankalpan <162314885+sankalpan@users.noreply.github.com>
2025-10-07 18:37:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,13 @@ app.use("/api/bookmarks", bookmarkRoutes);
 app.get('/', (req, res) => {
   res.send('API is running');
 });
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(process.env.PORT || 5000, () => console.log("Server is running!!")))
     .catch(err => console.error(err));
